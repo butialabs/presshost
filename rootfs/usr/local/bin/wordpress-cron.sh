d5f89d689a3b90ecfa00b256363bfdb52e8abadc
Renan Bernordi <altendorfme@gmail.com>
2026-05-21 15:02:21 -0300

    fix logs and levels

--- rootfs/usr/local/bin/wordpress-cron.sh
+++ rootfs/usr/local/bin/wordpress-cron.sh
@@ -5,22 +5,19 @@ SCRIPT_NAME="WORDPRESS-CRON"
 APP_USER="${APP_USER:-www-data}"
 APP_PATH="${APP_PATH:-/site/press}"
 LOG_FILE="${LOGS_PATH:-/site/logs}/cron-presshost.log"
-LOG_LEVEL="${LOG_LEVEL:-INFO}"
+LOG_LEVEL=INFO
 source "/usr/local/bin/common-utils.sh"
 
-info "WordPress cron started" >> "${LOG_FILE}" 2>&1
-trap 'info "WordPress cron finished" >> "${LOG_FILE}" 2>&1' EXIT
+info "Cron started" >> "${LOG_FILE}" 2>&1
 
 if [[ ! -f "${APP_PATH}/wp-load.php" ]]; then
     info "No Press installation detected at ${APP_PATH}!" >> "${LOG_FILE}" 2>&1
     exit 0
 fi
 
-info "Running WordPress cron events as ${APP_USER}" >> "${LOG_FILE}" 2>&1
-
 if runuser -u "${APP_USER}" -- /usr/local/bin/wp --path="${APP_PATH}" cron event run --due-now >> "${LOG_FILE}" 2>&1; then
-    success "WordPress cron events completed successfully" >> "${LOG_FILE}" 2>&1
+    success "Cron events completed successfully" >> "${LOG_FILE}" 2>&1
 else
-    error "Failed to run WordPress cron events" >> "${LOG_FILE}" 2>&1
+    error "Failed to run Cron events" >> "${LOG_FILE}" 2>&1
     exit 1
 fi
